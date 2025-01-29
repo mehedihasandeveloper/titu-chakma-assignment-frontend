faa3799034ba0dd8898635cee88b72c341a18374
Mehedi Hasan <mh.ju1803@gmail.com>
2025-01-29 07:43:44 +0600

    working product add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InsertProductComponent } from './components/insert-product/insert-product.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'product-crud-view', component: InsertProductComponent},
+  {path: 'login-page', component: LoginPageComponent},
+  {path: 'view-product', component: ViewProductsComponent},
+  {path: 'create-user', component: CreateUserComponent},
+  {path: '**', component: LoginPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { InsertProductComponent } from './components/insert-product/insert-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewProductsComponent,
+    CreateUserComponent,
+    LoginPageComponent,
+    InsertProductComponent
   ],
   imports: [
     BrowserModule,
